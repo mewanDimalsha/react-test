b13998bff1db46c7e9869fefb88dc8bb696d0d56
Mewan Dimalsha <115539818+mewanDimalsha@users.noreply.github.com>
2023-09-17 21:05:59 +0530

    list is added

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+function Message () {
+    // JSX: Javascript XML
+    const name= "";
+    if(name)
+        return <h1>hello {name}</h1>;
+    else
+        return <h1>hello world</h1>; 
+}
+
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,23 @@
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London"];
+
+  return (
+    <>
+      <h1>hello world!</h1>
+      {items.length === 0 && <p>No item Found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
